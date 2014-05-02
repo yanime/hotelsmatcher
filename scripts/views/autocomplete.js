3e02bfd22773f23e02ba32eb58021072ccbbe241
Andrei Sebastian Cîmpean <andreiamenta@gmail.com>
2014-05-02 07:17:15 +0300

    Fix issue that caused auto-complete to break after picking option.

--- scripts/views/autocomplete.js
+++ scripts/views/autocomplete.js
@@ -71,10 +71,9 @@ define([
             for(results in v) {
                 if(v.hasOwnProperty(results)){
                     className = 'action result ' + results + ' separated ';
-                    debugger;
                     for(i = 0, l = v[results].length; i < l; i++){
                         r = v[results][i];
-                        str += '<li data-value="'+r.destinationId+'" class="'+className+'">'+this._buildCountryWrapHelper(results, r.country)+r.name+'</li>';
+                        str += '<li data-value="'+r.destinationId+'" class="'+className+'">'+r.name+this._buildCountryWrapHelper(results, r.country)+'</li>';
                         className = 'action result ' + results;
                     }
                 }
