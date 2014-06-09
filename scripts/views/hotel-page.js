a1ce95b6be81f49de5102d7bf73c006fb829f727
Andrei Sebastian Cîmpean <andreiamenta@gmail.com>
2014-06-09 16:56:17 +0300

    Block hotel page when pressing the compare button.

--- scripts/views/hotel-page.js
+++ scripts/views/hotel-page.js
@@ -18,16 +18,23 @@ define([
             'click .close': '_handleClosePage',
             'click .compare': '_handleCompareRequest'
         },
+        block: function () {
+            that.$el.find('.loading').removeClass('hidden');
+        },
+        unblock: function () {
+            that.$el.find('.loading').addClass('hidden');
+        },
         afterRender: function () {
             var that = this;
             this.model.fetch().done(function () {
                 var view = new HotelView({model: that.model});
-                that.$el.find('.loading').remove();
                 that.insertView('#main-section', view);
                 view.render();
+                that.unblock();
             });
         },
         _handleCompareRequest: function () {
+            this.block();
             App.Search.fetchHotelsNearHotel(this.model).done(function () {
                 App.router.navigate('compare',{trigger: true});
             });
