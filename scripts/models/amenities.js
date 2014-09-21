e95d6d74f39f41a92e78d429473a733ef2d2f91b
VictorAlbulescu <victor.albulescu@epoint.ro>
2014-09-21 20:55:11 +0300

    New things

---
+++ scripts/models/amenities.js
@@ -0,0 +1,25 @@
+define([
+],
+function() {
+    var amenities = function () {
+        this.selected = [];
+    };
+
+    amenities.prototype.select = function(option) {
+        this.selected[option] = true;
+    };
+
+    amenities.prototype.select = function(option) {
+        this.selected[option] = true;
+    };
+
+    amenities.options = [{
+        name: 'Business Center',
+        check: function () {
+        }
+    },
+    ]
+
+
+    return amenities;
+});
